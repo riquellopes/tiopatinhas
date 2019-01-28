28983e5e6cd41c8b1ccc115ae5f974887c18ffe6
Henrique Lopes <riquellopes@gmail.com>
2019-01-28 19:34:26 -0200

    creating a test to validate step

--- organizze/api_test.go
+++ organizze/api_test.go
@@ -1,6 +1,7 @@
 package organizze
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -24,4 +25,18 @@ func Test_should_get_a_list_of_itens(t *testing.T) {
 	result, _ := oozz.GetTransactions()
 
 	assert.Equal(t, result, response)
+}
+
+func Test_should_get_error(t *testing.T) {
+	defer gock.Off()
+
+	gock.New(URI).
+		Get("/transactions").
+		Reply(200).
+		SetError(errors.New("server error"))
+
+	oozz := Organizze{User: "lopes@example.com", PasswordKey: "xxxxxxxxxxxxxxxx", Name: "Rick"}
+	_, error := oozz.GetTransactions()
+
+	assert.Error(t, error)
 }
